662bdfa195c8a8b61c1dcbaf61dcdf40439d4532
Phil Middleton <pjmiddle@mtu.edu>
2014-10-27 23:12:13 -0400

    Updated pywow to be multiple classes

--- pywow.py
+++ pywow.py
@@ -1,40 +1,95 @@
 #!/usr/bin/env python
-# Filename: pywow.pywow
+# Filename: pywow.py
 
 #imports
 import requests
 import json
 from config import realm_relation
 
-class Pywow(object):
+class Realm(object):
 	'''
 	Class description
 	'''
 
 	# Class Variables
-	__base_url = "https://us.battle.net/api/wow/"
+	__base_url = "https://us.battle.net/api/wow/realm/status"
 	__realms = realm_relation
 
-	def __get_realm_status(self, url, data, headers):
-		r = requests.get(url, headers=headers)
+	def __init__(self, name):
+		self.name = name
+
+	def __get_realm(self):
+		return self.__realms[self.name]
+
+	def __get_realm_data(self, url):
+		r = requests.get(url)
+		r = r.json()
 		return r
 
-	def get_realm_status(self, name):
+
+	def get_realm_status(self):
 		'''
 		Function definition
 		@return: True if the realm is up or False if the realm is down
 		'''
-		realm_lookup = self.__realms[name]
-		url = self.__base_url + 'realm/status'
-		headers = None
-		data = None
 
-		# Get the current status of all realms
-		results = self.__get_realm_status(url, data, headers)
-		results = results.json()
+		# Lookup the realm in realm_relation to get the index of the realm
+		# For the query that we are going to perform on the api
+		realm_lookup = self.__get_realm()
+
+		# Get the current stats of all realms
+		results = self.__get_realm_data(self.__base_url)
 
 		# Status of the realm
 		return results['realms'][realm_lookup]['status']
+
+
+	def get_realm_population(self):
+		'''
+		Function definition
+		@return: A string representing the realm get_realm_population
+		'''
+
+		# lookup the realm in the realm_relation to get the index of the realm
+		# for the query that we are going to perform on the api.
+		realm_lookup = self.__get_realm()
+
+		# Get the current stats of all realms
+		results = self.__get_realm_data(self.__base_url)
+
+		# Population of the realm
+		return results['realms'][realm_lookup]['population']
+
+
+	def get_connected_realms(self):
+		'''
+		Function definition
+		@return: an array of connected realms
+		'''
+
+		# lookup the realm in the realm_relation to get the index of the realm
+		# for the query that we are going to perform on the api.
+		realm_lookup = self.__get_realm()
+
+		# Get the current stats of all realms
+		results = self.__get_realm_data(self.__base_url)
+
+		# Array of connected realms
+		connected_realms = results['realms'][realm_lookup]['connected_realms']
+		
+		# Delete the first element of the list because it is itself
+		del connected_realms[0]
+
+		return connected_realms
+
+
+
+
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,19 +1,47 @@
-from pywow import Pywow
+from pywow import Realm
 
+def test_prep():
+	api = Realm("Bloodhoof")
+	return api
 
 def test_a():
-	api = Pywow()
-	test_a = api.get_realm_status('Bloodhoof')
+	api = test_prep()
+	test_a = api.get_realm_status()
 
 	if(test_a == True or test_a == False):
 		return "Test A: Pass - realm status: " + str(test_a)
 	else:
 		return "Test B: Failed"
 
+def test_b():
+	api = test_prep()
+	test_b = api.get_realm_population()
+
+	if(test_b != ""):
+		return "Test B Passed - Realm Population: " + test_b
+	else:
+		return "Test B Failed with an error"
+
+def test_c():
+	api = test_prep()
+	test_c = api.get_connected_realms()
+
+	if not test_c:
+		return "Has no connected realms or failed"
+	else:
+		return test_c
+
+
 def main():
 	first_test = test_a()
 	print first_test
 
+	second_test = test_b()
+	print second_test
+
+	third_test = test_c()
+	print third_test
+	
 if __name__ == '__main__':
 	main()
 
